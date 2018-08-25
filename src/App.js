e772fb04762b0689f8a8d2851bb0efce67126732
Eduardo Lucas <eduardolucas40@gmail.com>
2018-08-25 18:16:14 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+
+import { Provider } from "./context";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <AddContact />
+          <Contacts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
