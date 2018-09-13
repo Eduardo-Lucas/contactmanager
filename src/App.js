2b3b8f5d3206595806d353dae663ffa28f897025
Eduardo Lucas <eduardolucas40@gmail.com>
2018-09-13 08:23:21 -0300

    formatador corrigido

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
 
-import {
-  Provider
-} from "./context";
+import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -15,19 +12,19 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return ( <
-      Provider >
-      <
-      div className = "App" >
-      <
-      Header branding = "Contact Manager" / >
-      <
-      AddContact / >
-      <
-      Contacts / >
-      <
-      /div> <
-      /Provider>
+    return (
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
